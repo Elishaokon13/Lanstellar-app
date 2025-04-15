82b07fe9d74e4bcf2e06757fc8b9c1a5f4f5f589
Elisha David <eliscodavid@gmail.com>
2025-04-15 15:48:25 +0100

    Create page.tsx

---
+++ app/settings/page.tsx
@@ -0,0 +1,10 @@
+import Settings from "@/components/lanstellarui/settings"
+import Layout from "@/components/lanstellarui/layout"
+
+export default function SettingsPage() {
+  return (
+    <Layout>
+      <Settings />
+    </Layout>
+  )
+}
